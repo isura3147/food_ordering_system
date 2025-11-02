1f333b3fc1400f2b2b5b9a8b78bbdafb8ac145a8
isura <isura3147@gmail.com>
2025-11-02 13:44:55 +0530

    removed orders files, added data when creating database.

--- src/main/java/edu/icet/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package edu.icet.repository;
-
-import edu.icet.model.entity.OrderEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<OrderEntity, String> {
-}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO item VALUES(3000, 105.00, 'P001', '1kg', 'Keerisamba Retail');
+INSERT INTO item VALUES(200, 525.00, 'P002', '5kg', 'Keerisamba 5kg');
