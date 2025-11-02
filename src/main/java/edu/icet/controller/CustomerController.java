f2a055c946b3f7fcb70932297249cbf9c9ba591a
isura <isura3147@gmail.com>
2025-11-02 10:59:08 +0530

    Add register customer API

---
+++ src/main/java/edu/icet/controller/CustomerController.java
@@ -0,0 +1,22 @@
+package edu.icet.controller;
+
+import edu.icet.model.dto.Customer;
+import edu.icet.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/customers")
+public class CustomerController {
+
+    @Autowired
+    CustomerService customerService;
+
+    @PostMapping("/register")
+    public void addCustomer(@RequestBody Customer customer) {
+        System.out.println(customerService.addCustomer(customer));
+    }
+}
---
+++ src/main/java/edu/icet/service/CustomerService.java
@@ -0,0 +1,26 @@
+package edu.icet.service;
+
+import edu.icet.model.dto.Customer;
+import edu.icet.model.entity.CustomerEntity;
+import edu.icet.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class CustomerService {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    public String addCustomer(Customer customer) {
+        customerRepository.save(new CustomerEntity(
+                customer.getId(),
+                customer.getTitle(),
+                customer.getName(),
+                customer.getPhoneNumber()
+        ));
+        return "All good mate";
+    }
+
+}
